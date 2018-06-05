ca04403cfa4fe9c14fbb527b4fb988d876585d30
SonLe <lequocson1997@gmail.com>
2018-06-05 12:04:29 +0700

    Them Sinh Vien

--- WindowsFormsApp1/ThemSV.Designer.cs
+++ WindowsFormsApp1/ThemSV.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace LibraryManagement
+﻿namespace WindowsFormsApp1
 {
-    partial class Form1
+    partial class ThemSV
     {
         /// <summary>
         /// Required designer variable.
@@ -30,14 +30,14 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // ThemSV
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Name = "ThemSV";
+            this.Text = "ThemSV";
+            this.Load += new System.EventHandler(this.ThemSV_Load);
             this.ResumeLayout(false);
 
         }
@@ -45,4 +45,3 @@
         #endregion
     }
 }
-
---
+++ WindowsFormsApp1/ThemSV.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManagement
+{
+    public partial class ThemSV : Form
+    {
+        QuanLyThuVienEntities db = new QuanLyThuVienEntities();
+        public ThemSV()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            var rs = new SinhVien
+            {
+                Hoten = txtName.Text,
+                MSSV = txtMSSV.Text,
+                Khoa = cbbKhoa.Text,
+            };
+            db.SinhViens.Add(rs);
+            db.SaveChanges();
+            this.Close();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ThemSV_Load(object sender, EventArgs e)
+        {
+            var lst = db.Khoas.ToList();
+            foreach (var item in lst)
+            {
+                cbbKhoa.Items.Add(new { Text = item.TenKhoa, Value = "1" });
+            }
+
+            cbbKhoa.DisplayMember = "Text";
+            cbbKhoa.ValueMember = "Value";
+        }
+    }
+}
